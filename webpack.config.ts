ba70bf880451faa80c7eddbffc2f358ba1f1286b
Giang Huynh <trgianghuynhdev1808@gmail.com>
2022-03-16 19:47:19 +0700

    hotfix: wrong entry client webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,12 @@
-import { DefinePlugin, Configuration } from "webpack";
-import "webpack-dev-server";
-import path from "path";
-import { CleanWebpackPlugin } from "clean-webpack-plugin";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import ReactRefreshPlugin from "@pmmmwh/react-refresh-webpack-plugin";
-import { merge } from "webpack-merge";
-import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin";
-import CopyWebpackPlugin from "copy-webpack-plugin";
+import { DefinePlugin, Configuration } from 'webpack';
+import 'webpack-dev-server';
+import path from 'path';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { merge } from 'webpack-merge';
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 interface Env {
   production: boolean;
@@ -15,33 +15,33 @@ interface Env {
 
 function createBaseConfig(env: Env): Configuration {
   return {
-    mode: env.production ? "production" : "development",
+    mode: env.production ? 'production' : 'development',
 
-    devtool: env.production ? false : "source-map",
+    devtool: env.production ? false : 'source-map',
 
     resolve: {
-      extensions: [".ts", ".tsx", ".js"],
-      plugins: [new TsconfigPathsPlugin()],
+      extensions: ['.ts', '.tsx', '.js'],
+      plugins: [new TsconfigPathsPlugin()]
     },
 
     plugins: [
       new DefinePlugin({
-        __PRODUCTION__: JSON.stringify(env.production),
-      }),
-    ],
+        __PRODUCTION__: JSON.stringify(env.production)
+      })
+    ]
   };
 }
 
 function createServerConfig(env: Env): Configuration {
   return {
-    name: "server",
+    name: 'server',
 
-    target: "node",
+    target: 'node',
 
-    context: path.resolve(__dirname, "src/server"),
+    context: path.resolve(__dirname, 'src/server'),
 
     externalsPresets: {
-      node: true,
+      node: true
     },
 
     ignoreWarnings: [
@@ -52,47 +52,46 @@ function createServerConfig(env: Env): Configuration {
          * more at: https://github.com/webpack/webpack/issues/1576
          */
         module: /express/,
-        message:
-          /Critical\sdependency:\sthe\srequest\sof\sa\sdependency\sis\san\sexpression/,
-      },
+        message: /Critical\sdependency:\sthe\srequest\sof\sa\sdependency\sis\san\sexpression/
+      }
     ],
 
-    entry: "./app.ts",
+    entry: './app.ts',
 
     output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "app.js",
-      publicPath: "./",
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'app.js',
+      publicPath: './'
     },
 
     module: {
       rules: [
-        { test: /\.tsx?$/, loader: "ts-loader", exclude: /node_modules/ },
-        { test: /\.scss$/, loader: "ignore-loader" },
+        { test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/ },
+        { test: /\.scss$/, loader: 'ignore-loader' },
         {
           // file-loader config must match client's (except 'emitFile' property)
           test: /\.(jpg|png|gif|svg)$/,
           use: {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              outputPath: "images",
-              name: "[name].[contenthash].[ext]",
-              emitFile: false,
-            },
-          },
-        },
-      ],
+              outputPath: 'images',
+              name: '[name].[contenthash].[ext]',
+              emitFile: false
+            }
+          }
+        }
+      ]
     },
 
     plugins: [
       new CleanWebpackPlugin({
-        cleanOnceBeforeBuildPatterns: ["!public/**"],
+        cleanOnceBeforeBuildPatterns: ['!public/**']
       }),
 
       new DefinePlugin({
-        __Server__: JSON.stringify(true),
-      }),
-    ],
+        __Server__: JSON.stringify(true)
+      })
+    ]
   };
 }
 
@@ -100,37 +99,33 @@ function createClientConfig(env: Env): Configuration {
   const CLIENT_PORT = 9000;
 
   const babelConfig = {
-    presets: [
-      "@babel/preset-env",
-      "@babel/preset-react",
-      "@babel/preset-typescript",
-    ],
+    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
     plugins: [
-      "@babel/plugin-transform-runtime",
-      env.hot && require.resolve("react-refresh/babel"),
-    ].filter(Boolean),
+      '@babel/plugin-transform-runtime',
+      env.hot && require.resolve('react-refresh/babel')
+    ].filter(Boolean)
   };
 
   return {
-    name: "client",
+    name: 'client',
 
-    target: "web",
+    target: 'web',
 
-    context: path.resolve(__dirname, "src/client"),
+    context: path.resolve(__dirname, 'src/client'),
 
     optimization: {
       splitChunks: {
-        chunks: "all",
-      },
+        chunks: 'all'
+      }
     },
 
     entry: {
-      index: "./Index.tsx",
+      index: './index.tsx'
     },
 
     output: {
-      path: path.resolve(__dirname, "dist", "public"),
-      filename: env.production ? "js/[name].[chunkhash].js" : "js/[name].js",
+      path: path.resolve(__dirname, 'dist', 'public'),
+      filename: env.production ? 'js/[name].[chunkhash].js' : 'js/[name].js'
     },
 
     module: {
@@ -138,61 +133,61 @@ function createClientConfig(env: Env): Configuration {
         {
           test: /\.tsx?$/,
           exclude: /node_modules/,
-          use: { loader: "babel-loader", options: babelConfig },
+          use: { loader: 'babel-loader', options: babelConfig }
         },
         {
           test: /\.scss$/,
           use: [
-            { loader: "style-loader" }, // to inject the result into the DOM as a style block
-            { loader: "css-modules-typescript-loader" }, // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
-            { loader: "css-loader", options: { modules: true } }, // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
-            { loader: "sass-loader" }, // to convert SASS to CSS
+            { loader: 'style-loader' }, // to inject the result into the DOM as a style block
+            { loader: 'css-modules-typescript-loader' }, // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
+            { loader: 'css-loader', options: { modules: true } }, // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
+            { loader: 'sass-loader' } // to convert SASS to CSS
             // NOTE: The first build after adding/removing/renaming CSS classes fails, since the newly generated .d.ts typescript module is picked up only later
-          ],
+          ]
         },
         {
           test: /\.(jpg|png|gif|svg)$/,
           use: {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              outputPath: "images",
-              name: "[name].[contenthash].[ext]",
-            },
-          },
-        },
-      ],
+              outputPath: 'images',
+              name: '[name].[contenthash].[ext]'
+            }
+          }
+        }
+      ]
     },
 
     plugins: [
       new CleanWebpackPlugin(),
 
       new HtmlWebpackPlugin({
-        template: "./index.html",
+        template: './index.html'
       }),
 
       new CopyWebpackPlugin({
-        patterns: [{ from: "resources/favicon.ico" }],
+        patterns: [{ from: 'resources/favicon.ico' }]
       }),
 
       new DefinePlugin({
-        __SERVER__: JSON.stringify(false),
+        __SERVER__: JSON.stringify(false)
       }),
 
-      (env.hot && new ReactRefreshPlugin()) as any,
+      (env.hot && new ReactRefreshPlugin()) as any
     ].filter(Boolean),
 
     devServer: {
       hot: env.hot,
       port: CLIENT_PORT,
-      historyApiFallback: true,
-    },
+      historyApiFallback: true
+    }
   };
 }
 
 export default function (args: Record<string, string>) {
   const env: Env = {
-    hot: !!args["HOT"],
-    production: !!args["PRODUCTION"],
+    hot: !!args['HOT'],
+    production: !!args['PRODUCTION']
   };
 
   const baseConfig = createBaseConfig(env);
